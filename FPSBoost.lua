0c4df66cffa104efc97d8045c28c48f040ab9385
bostonstrong567 <32438405+bostonstrong567@users.noreply.github.com>
2023-09-19 18:35:52 -0400

    Update FPSBoost.lua

--- FPSBoost.lua
+++ FPSBoost.lua
@@ -12,8 +12,8 @@ local connections = {}
 
 local function storeOriginalValues()
     local originalValues = {}
-    for _, v in pairs(game.Workspace:GetDescendants()) do
-        if v:IsA("BasePart") and not v:IsA("Texture") then
+    for _, v in pairs(workspace:FindPartsInRegion3(workspace:FindPartOnRay(Ray.new(workspace.Position, Vector3.new(0, -10000, 0)), nil, false).Position + Vector3.new(0, 10000, 0), nil, true)) do
+        if v:IsA("BasePart") then
             originalValues[v] = { Material = v.Material, Transparency = v.Transparency }
         elseif v:IsA("Decal") or v:IsA("Texture") then
             originalValues[v] = { Transparency = v.Transparency }
@@ -23,7 +23,7 @@ local function storeOriginalValues()
             originalValues[v] = { Enabled = v.Enabled }
         end
     end
-    for _, e in ipairs(game.Lighting:GetChildren()) do
+    for _, e in pairs(game.Lighting:GetChildren()) do
         if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
             originalValues[e] = { Enabled = e.Enabled }
         end
@@ -33,72 +33,66 @@ end
 
 FPSBoost.originalValues = storeOriginalValues()
 
-function FPSBoost:modifyDescendant(descendant)
-    local originalValues = self.originalValues[descendant] or {}
-
-    if descendant:IsA("BasePart") and not descendant:IsA("Texture") then
-        if self.settings.LowQualityParts then
-            descendant.Material = Enum.Material.SmoothPlastic
-            descendant.Reflectance = 0
-        else
-            descendant.Material = originalValues.Material or descendant.Material
-            descendant.Transparency = originalValues.Transparency or descendant.Transparency
-        end
-    end
-
-    if descendant:IsA("Decal") or descendant:IsA("Texture") then
-        if self.settings.RemoveDecalsAndTextures then
-            descendant.Transparency = 1
-        else
-            descendant.Transparency = originalValues.Transparency or descendant.Transparency
-        end
-    end
-
-    if descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
-        if self.settings.DisableParticles then
-            descendant.Enabled = false
-        else
-            descendant.Enabled = originalValues.Enabled or descendant.Enabled
-        end
-    end
-
-    if descendant:IsA("Light") then
-        if self.settings.DisableLights then
-            descendant.Enabled = false
-        else
-            descendant.Enabled = originalValues.Enabled or descendant.Enabled
-        end
-    end
-
-    self.originalValues[descendant] = originalValues
-end
-
-function FPSBoost:applySettings()
+function FPSBoost:toggleLowQualityParts()
     for descendant, originalValues in pairs(self.originalValues) do
-        self:modifyDescendant(descendant)
-    end
-
-    for _, e in ipairs(game.Lighting:GetChildren()) do
-        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
-            if self.settings.DisableEffects then
-                e.Enabled = false
+        if descendant:IsA("BasePart") and not descendant:IsA("Texture") then
+            if self.settings.LowQualityParts then
+                descendant.Material = Enum.Material.SmoothPlastic
+                descendant.Transparency = 1
             else
-                e.Enabled = originalValues and originalValues.Enabled or true
+                descendant.Material = originalValues.Material
+                descendant.Transparency = originalValues.Transparency
             end
         end
     end
 end
 
-function FPSBoost:setupConnections()
-    if connections.descendantAdded then
-        connections.descendantAdded:Disconnect()
+function FPSBoost:toggleDecalsAndTextures()
+    for descendant, originalValues in pairs(self.originalValues) do
+        if descendant:IsA("Decal") or descendant:IsA("Texture") then
+            if self.settings.RemoveDecalsAndTextures then
+                descendant.Transparency = 1
+            else
+                descendant.Transparency = originalValues.Transparency
+            end
+        end
     end
-
-    connections.descendantAdded = game.Workspace.DescendantAdded:Connect(function(descendant)
-        self:modifyDescendant(descendant)
-    end)
 end
 
-FPSBoost:setupConnections()
+function FPSBoost:toggleParticles()
+    for descendant, originalValues in pairs(self.originalValues) do
+        if descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
+            if self.settings.DisableParticles then
+                descendant.Enabled = false
+            else
+                descendant.Enabled = originalValues.Enabled
+            end
+        end
+    end
+end
+
+function FPSBoost:toggleLights()
+    for descendant, originalValues in pairs(self.originalValues) do
+        if descendant:IsA("Light") then
+            if self.settings.DisableLights then
+                descendant.Enabled = false
+            else
+                descendant.Enabled = originalValues.Enabled
+            end
+        end
+    end
+end
+
+function FPSBoost:toggleEffects()
+    for _, e in pairs(game.Lighting:GetChildren()) do
+        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+            if self.settings.DisableEffects then
+                e.Enabled = false
+            else
+                e.Enabled = self.originalValues[e] and self.originalValues[e].Enabled or true
+            end
+        end
+    end
+end
 
 return FPSBoost
