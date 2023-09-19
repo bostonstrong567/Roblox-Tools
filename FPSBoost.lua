a9f9df8f212c7447ce043c1ec1561ba67a00b094
bostonstrong567 <32438405+bostonstrong567@users.noreply.github.com>
2023-09-19 18:06:33 -0400

    Update FPSBoost.lua

--- FPSBoost.lua
+++ FPSBoost.lua
@@ -19,6 +19,8 @@ local function storeOriginalValues()
             originalValues[v] = {Transparency = v.Transparency}
         elseif v:IsA("Light") then
             originalValues[v] = {IsEnabled = v.Enabled}
+        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+            originalValues[v] = {IsEnabled = v.Enabled}
         end
     end
     for i, e in ipairs(game.Lighting:GetChildren()) do
@@ -34,51 +36,35 @@ FPSBoost.originalValues = storeOriginalValues()
 function FPSBoost:modifyDescendant(descendant)
     if descendant:IsA("BasePart") then
         if self.settings.LowQualityParts then
-            descendant.Material = "SmoothPlastic"
+            descendant.Material = Enum.Material.SmoothPlastic
         else
             descendant.Material = self.originalValues[descendant] and self.originalValues[descendant].Material or descendant.Material
-        end
-    end
-
-    if descendant:IsA("Decal") or descendant:IsA("Texture") then
-        if self.settings.RemoveDecalsAndTextures then
-            descendant.Transparency = 1
-        else
             descendant.Transparency = self.originalValues[descendant] and self.originalValues[descendant].Transparency or descendant.Transparency
         end
     end
 
-    if descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
-        if self.settings.DisableParticles then
-            descendant.Enabled = false
-        else
-            descendant.Enabled = true
+    if (descendant:IsA("Decal") or descendant:IsA("Texture")) and self.settings.RemoveDecalsAndTextures then
+        descendant.Transparency = 1
+    end
+
+    if (descendant:IsA("ParticleEmitter") or descendant:IsA("Trail")) and self.settings.DisableParticles then
+        descendant.Enabled = false
+    end
+
+    if descendant:IsA("Light") and self.settings.DisableLights then
+        descendant.Enabled = false
+    end
+
+    -- Store the original values of the new descendant
+    if not self.originalValues[descendant] then
+        if descendant:IsA("BasePart") then
+            self.originalValues[descendant] = {Material = descendant.Material, Transparency = descendant.Transparency}
+        elseif descendant:IsA("Decal") or descendant:IsA("Texture") then
+            self.originalValues[descendant] = {Transparency = descendant.Transparency}
+        elseif descendant:IsA("Light") or descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
+            self.originalValues[descendant] = {IsEnabled = descendant.Enabled}
         end
     end
-
-    if descendant:IsA("Light") then
-        if self.settings.DisableLights then
-            descendant.Enabled = false
-        else
-            descendant.Enabled = self.originalValues[descendant] and self.originalValues[descendant].IsEnabled or descendant.Enabled
-        end
-    end
-end
-
-function FPSBoost:applySettings()
-    for i, v in pairs(game.Workspace:GetDescendants()) do
-        self:modifyDescendant(v)
-    end
-end
-
-function FPSBoost:setupConnections()
-    if connections.descendantAdded then
-        connections.descendantAdded:Disconnect()
-    end
-
-    connections.descendantAdded = game.Workspace.DescendantAdded:Connect(function(descendant)
-        FPSBoost:modifyDescendant(descendant)
-    end)
 end
 
 FPSBoost:setupConnections()
