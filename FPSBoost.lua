1127a34bdc558b406a367d1056158f99dfbb607e
bostonstrong567 <32438405+bostonstrong567@users.noreply.github.com>
2023-09-19 18:25:11 -0400

    Update FPSBoost.lua

--- FPSBoost.lua
+++ FPSBoost.lua
@@ -12,7 +12,7 @@ local connections = {}
 
 local function storeOriginalValues()
     local originalValues = {}
-    for i, v in pairs(game.Workspace:GetDescendants()) do
+    for _, v in pairs(game.Workspace:GetDescendants()) do
         if v:IsA("BasePart") and not v:IsA("Texture") then
             originalValues[v] = { Material = v.Material, Transparency = v.Transparency }
         elseif v:IsA("Decal") or v:IsA("Texture") then
@@ -23,7 +23,7 @@ local function storeOriginalValues()
             originalValues[v] = { Enabled = v.Enabled }
         end
     end
-    for i, e in ipairs(game.Lighting:GetChildren()) do
+    for _, e in ipairs(game.Lighting:GetChildren()) do
         if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
             originalValues[e] = { Enabled = e.Enabled }
         end
@@ -34,120 +34,56 @@ end
 FPSBoost.originalValues = storeOriginalValues()
 
 function FPSBoost:modifyDescendant(descendant)
-    local originalValues = self.originalValues[descendant]
+    local originalValues = self.originalValues[descendant] or {}
 
     if descendant:IsA("BasePart") and not descendant:IsA("Texture") then
-        if not originalValues then
-            originalValues = { Material = descendant.Material, Transparency = descendant.Transparency }
-            self.originalValues[descendant] = originalValues
-        end
-
         if self.settings.LowQualityParts then
             descendant.Material = Enum.Material.SmoothPlastic
             descendant.Reflectance = 0
         else
-            descendant.Material = originalValues.Material
-            descendant.Transparency = originalValues.Transparency
+            descendant.Material = originalValues.Material or descendant.Material
+            descendant.Transparency = originalValues.Transparency or descendant.Transparency
         end
     end
 
-    if (descendant:IsA("Decal") or descendant:IsA("Texture")) then
-        if not originalValues then
-            originalValues = { Transparency = descendant.Transparency }
-            self.originalValues[descendant] = originalValues
-        end
-
+    if descendant:IsA("Decal") or descendant:IsA("Texture") then
         if self.settings.RemoveDecalsAndTextures then
             descendant.Transparency = 1
         else
-            descendant.Transparency = originalValues.Transparency
+            descendant.Transparency = originalValues.Transparency or descendant.Transparency
         end
     end
 
-    if (descendant:IsA("ParticleEmitter") or descendant:IsA("Trail")) then
-        if not originalValues then
-            originalValues = { Enabled = descendant.Enabled }
-            self.originalValues[descendant] = originalValues
-        end
-
+    if descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
         if self.settings.DisableParticles then
             descendant.Enabled = false
         else
-            descendant.Enabled = originalValues.Enabled
+            descendant.Enabled = originalValues.Enabled or descendant.Enabled
         end
     end
 
-    if (descendant:IsA("PointLight") or descendant:IsA("SpotLight") or descendant:IsA("SurfaceLight")) then
-        if not originalValues then
-            originalValues = { Enabled = descendant.Enabled }
-            self.originalValues[descendant] = originalValues
-        end
-
+    if descendant:IsA("Light") then
         if self.settings.DisableLights then
             descendant.Enabled = false
         else
-            descendant.Enabled = originalValues.Enabled
+            descendant.Enabled = originalValues.Enabled or descendant.Enabled
         end
     end
+
+    self.originalValues[descendant] = originalValues
 end
 
 function FPSBoost:applySettings()
     for descendant, originalValues in pairs(self.originalValues) do
-        if descendant then
-            if descendant:IsA("BasePart") and not descendant:IsA("Texture") then
-                if self.settings.LowQualityParts then
-                    descendant.Material = Enum.Material.SmoothPlastic
-                    descendant.Reflectance = 0
-                else
-                    descendant.Material = originalValues.Material
-                    descendant.Transparency = originalValues.Transparency
-                end
-            end
-
-            if (descendant:IsA("Decal") or descendant:IsA("Texture")) then
-                if self.settings.RemoveDecalsAndTextures then
-                    descendant.Transparency = 1
-                else
-                    descendant.Transparency = originalValues.Transparency
-                end
-            end
-
-            if (descendant:IsA("ParticleEmitter") or descendant:IsA("Trail")) and originalValues.Enabled ~= nil then
-                if self.settings.DisableParticles then
-                    descendant.Enabled = false
-                else
-                    descendant.Enabled = originalValues.Enabled
-                end
-            end
-
-            if (descendant:IsA("PointLight") or descendant:IsA("SpotLight") or descendant:IsA("SurfaceLight")) and originalValues.Enabled ~= nil then
-                if self.settings.DisableLights then
-                    descendant.Enabled = false
-                else
-                    descendant.Enabled = originalValues.Enabled
-                end
-            end
-        end
+        self:modifyDescendant(descendant)
     end
 
-    for i, e in ipairs(game.Lighting:GetChildren()) do
+    for _, e in ipairs(game.Lighting:GetChildren()) do
         if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
             if self.settings.DisableEffects then
                 e.Enabled = false
             else
-                e.Enabled = self.originalValues[e] and self.originalValues[e].Enabled or true
-            end
-        end
-    end
-end
-
-
-    for i, e in ipairs(game.Lighting:GetChildren()) do
-        if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
-            if self.settings.DisableEffects then
-                e.Enabled = false
-            else
-                e.Enabled = self.originalValues[e] and self.originalValues[e].Enabled or true
+                e.Enabled = originalValues and originalValues.Enabled or true
             end
         end
     end
