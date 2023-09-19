7d55d9ceb934ea47b11554a45c4a72af86e9d5ce
bostonstrong567 <32438405+bostonstrong567@users.noreply.github.com>
2023-09-19 09:40:00 -0400

    Update FPSBoost.lua

--- FPSBoost.lua
+++ FPSBoost.lua
@@ -11,7 +11,6 @@ local workspace = game:GetService("Workspace")
 local Lighting = game:GetService("Lighting")
 
 FPSBoost.FPSBoostSettings = {
-    ["FPS Cap"] = nil,
     ["No Particles"] = false,
     ["No Camera Effects"] = false,
     ["No Explosions"] = false,
@@ -60,6 +59,7 @@ FPSBoost.AdvancedFPSBoostSettings = {
     },
 }
 
+FPSBoost.OriginalSettings = {}
 FPSBoost.originalInstanceSettings = {}
 
 local playerCharacters = {}
@@ -220,18 +220,29 @@ function FPSBoost:applyLowWaterGraphics()
         local lowWaterGraphics = self.FPSBoostSettings["Low Water Graphics"]
         local terrainSettings = settingsMapping.LowWaterGraphics
 
+        if not self.OriginalSettings.LowWaterGraphics then
+            self.OriginalSettings.LowWaterGraphics = {
+                WaterWaveSize = terrain.WaterWaveSize,
+                WaterWaveSpeed = terrain.WaterWaveSpeed,
+                WaterReflectance = terrain.WaterReflectance,
+                WaterTransparency = terrain.WaterTransparency,
+                Decoration = gethiddenproperty(terrain, "Decoration")
+            }
+        end
+
         if lowWaterGraphics then
             terrain.WaterWaveSize = 0
             terrain.WaterWaveSpeed = 0
             terrain.WaterReflectance = 0
             terrain.WaterTransparency = 0
             sethiddenproperty(terrain, "Decoration", false)
-        elseif terrainSettings.Decoration then
-            terrain.WaterWaveSize = terrainSettings.WaterWaveSize
-            terrain.WaterWaveSpeed = terrainSettings.WaterWaveSpeed
-            terrain.WaterReflectance = terrainSettings.WaterReflectance
-            terrain.WaterTransparency = terrainSettings.WaterTransparency
-            sethiddenproperty(terrain, "Decoration", terrainSettings.Decoration)
+        else
+            local original = self.OriginalSettings.LowWaterGraphics
+            terrain.WaterWaveSize = original.WaterWaveSize
+            terrain.WaterWaveSpeed = original.WaterWaveSpeed
+            terrain.WaterReflectance = original.WaterReflectance
+            terrain.WaterTransparency = original.WaterTransparency
+            sethiddenproperty(terrain, "Decoration", original.Decoration)
         end
     end
 end
@@ -266,17 +277,157 @@ function FPSBoost:applyLowRendering()
     end
 end
 
-function FPSBoost:applyFPSCap()
-    if self.FPSBoostSettings["FPS Cap"] == nil then
-        setfpscap(999)
+function FPSBoost:applyNoParticles()
+    if not self.OriginalSettings.NoParticles then
+        self.OriginalSettings.NoParticles = {}
     end
+
+    if self.FPSBoostSettings["No Particles"] then
+        for _, particle in pairs(workspace:FindPartsInRegion3(workspace:FindPartOnRay(Ray.new(workspace.Position, (workspace.Size + Vector3.new(1, 1, 1)) * 2)), nil, false)) do
+            if particle:IsA("ParticleEmitter") or particle:IsA("Trail") then
+                table.insert(self.OriginalSettings.NoParticles, {particle = particle, state = particle.Enabled})
+                
+                if self.AdvancedFPSBoostSettings.ParticleSettings.Destroy then
+                    particle:Destroy()
+                else
+                    particle.Enabled = not self.AdvancedFPSBoostSettings.ParticleSettings.Invisible
+                end
+            end
+        end
+    else
+        for _, data in pairs(self.OriginalSettings.NoParticles) do
+            if data.particle and data.particle:IsA("BasePart") then  -- Check if the particle still exists
+                data.particle.Enabled = data.state
+            end
+        end
+        self.OriginalSettings.NoParticles = {}  -- Clear the original settings after reverting
+    end
+end
+
+function FPSBoost:applyNoCameraEffects()
+    local StarterGui = game:GetService("StarterGui")
+    
+    if not self.OriginalSettings.NoCameraEffects then
+        self.OriginalSettings.NoCameraEffects = StarterGui:GetCoreGuiEnabled(Enum.CoreGuiType.All)
+    end
+    
+    if self.FPSBoostSettings["No Camera Effects"] then
+        StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
+    else
+        StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, self.OriginalSettings.NoCameraEffects)
+    end
+end
+
+
+function FPSBoost:applyNoExplosions()
+    if not self.OriginalSettings.NoExplosions then
+        self.OriginalSettings.NoExplosions = {}
+    end
+
+    workspace.DescendantAdded:Connect(function(item)
+        if item:IsA("Explosion") then
+            if self.FPSBoostSettings["No Explosions"] then
+                local originalSettings = {
+                    Visible = item.Visible,
+                    BlastPressure = item.BlastPressure,
+                    BlastRadius = item.BlastRadius
+                }
+                table.insert(self.OriginalSettings.NoExplosions, {item = item, settings = originalSettings})
+
+                if self.AdvancedFPSBoostSettings.ExplosionSettings.Destroy then
+                    item:Destroy()
+                else
+                    item.Visible = not self.AdvancedFPSBoostSettings.ExplosionSettings.Invisible
+                    item.BlastPressure = self.AdvancedFPSBoostSettings.ExplosionSettings.Smaller and 1 or item.BlastPressure
+                    item.BlastRadius = self.AdvancedFPSBoostSettings.ExplosionSettings.Smaller and 1 or item.BlastRadius
+                end
+            else
+                for _, data in pairs(self.OriginalSettings.NoExplosions) do
+                    if data.item and data.item:IsA("Explosion") then
+                        data.item.Visible = data.settings.Visible
+                        data.item.BlastPressure = data.settings.BlastPressure
+                        data.item.BlastRadius = data.settings.BlastRadius
+                    end
+                end
+                self.OriginalSettings.NoExplosions = {}
+            end
+        end
+    end)
+end
+
+function FPSBoost:applyNoClothes()
+    if not self.OriginalSettings.NoClothes then
+        self.OriginalSettings.NoClothes = {}
+    end
+
+    workspace.DescendantAdded:Connect(function(item)
+        if item:IsA("Clothing") or item:IsA("Pants") or item:IsA("Shirt") then
+            if self.FPSBoostSettings["No Clothes"] then
+                local originalParent = item.Parent
+                table.insert(self.OriginalSettings.NoClothes, {item = item, parent = originalParent})
+                item:Destroy()
+            else
+                for _, data in pairs(self.OriginalSettings.NoClothes) do
+                    if data.item then
+                        data.item.Parent = data.parent
+                    end
+                end
+                self.OriginalSettings.NoClothes = {}
+            end
+        end
+    end)
+end
+
+function FPSBoost:applyLowQualityParts()
+    if not self.OriginalSettings.LowQualityParts then
+        self.OriginalSettings.LowQualityParts = {}
+    end
+
+    workspace.DescendantAdded:Connect(function(item)
+        if item:IsA("Part") or item:IsA("MeshPart") then
+            if self.FPSBoostSettings["Low Quality Parts"] then
+                local originalSettings = {
+                    Material = item.Material,
+                    Reflectance = item.Reflectance,
+                    TextureID = item:IsA("MeshPart") and item.TextureID or nil,
+                    MeshId = item:IsA("MeshPart") and item.MeshId or nil
+                }
+                table.insert(self.OriginalSettings.LowQualityParts, {item = item, settings = originalSettings})
+
+                if self.AdvancedFPSBoostSettings.MeshPartSettings.LowerQuality then
+                    item.Material = Enum.Material.Plastic
+                    item.Reflectance = 0
+                end
+                if self.AdvancedFPSBoostSettings.MeshPartSettings.NoTexture then
+                    item.TextureID = ""
+                end
+                if self.AdvancedFPSBoostSettings.MeshPartSettings.NoMesh and item:IsA("MeshPart") then
+                    item.MeshId = ""
+                end
+                if self.AdvancedFPSBoostSettings.MeshPartSettings.Destroy then
+                    item:Destroy()
+                end
+            else
+                for _, data in pairs(self.OriginalSettings.LowQualityParts) do
+                    if data.item then
+                        data.item.Material = data.settings.Material
+                        data.item.Reflectance = data.settings.Reflectance
+                        if data.item:IsA("MeshPart") then
+                            data.item.TextureID = data.settings.TextureID
+                            data.item.MeshId = data.settings.MeshId
+                        end
+                    end
+                end
+                self.OriginalSettings.LowQualityParts = {}
+            end
+        end
+    end)
 end
 
 function FPSBoost:updateSettings()
     self:applyLowWaterGraphics()
     self:applyNoShadows()
     self:applyLowRendering()
-    self:applyFPSCap()
     print("FPSBoost settings updated.")
 end
 
@@ -284,7 +435,6 @@ function FPSBoost:initialize()
     self:applyLowWaterGraphics()
     self:applyNoShadows()
     self:applyLowRendering()
-    self:applyFPSCap()
 
     workspace.DescendantAdded:Connect(function(instance)
         CheckIfBad(instance)
